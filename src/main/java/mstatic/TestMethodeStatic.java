4d9b0d0fc24ca89660a33e132e8c95f7e22b32e5
Erico Labare <erico.labare@outlook.com>
2024-10-07 11:00:26 +0200

    update TP4

---
+++ src/main/java/mstatic/TestMethodeStatic.java
@@ -0,0 +1,16 @@
+package mstatic;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+
+        int chaintInt = Integer. parseInt(chaine);
+
+        int a = 4;
+        int b = 6;
+
+        int max = Integer.max(a, b);
+
+        System.out.print(max);
+    }
+}
