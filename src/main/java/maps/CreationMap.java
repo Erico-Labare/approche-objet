986acc6ddc1127c23b8dc83453c93a64d3f91364
Erico Labare <erico.labare@outlook.com>
2024-10-14 10:28:15 +0200

    update TP12 fin + misc)

--- src/main/java/maps/CreationMap.java
+++ src/main/java/maps/CreationMap.java
@@ -1,4 +1,41 @@
 package maps;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
 public class CreationMap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> mapSalaires = new HashMap<>();
+
+        mapSalaires.put("Paul", 1750);
+        mapSalaires.put("Hicham", 1825);
+        mapSalaires.put("Yu", 2250);
+        mapSalaires.put("Ingrid", 2015);
+        mapSalaires.put("Chantal", 2418);
+
+        System.out.println(mapSalaires);
+
+        HashMap<Integer, String> mapVilles = new HashMap<>();
+        mapVilles.put(13, "Marseille");
+        mapVilles.put(34, "Montpellier");
+        mapVilles.put(44, "Nantes");
+        mapVilles.put(75, "Paris");
+        mapVilles.put(31, "Toulouse");
+        mapVilles.put(59, "Lille");
+        mapVilles.put(69, "Lyon");
+        mapVilles.put(33, "Bordeaux");
+
+        System.out.print("\n");
+        for (Integer key: mapVilles.keySet()){
+            System.out.print(key+" " );
+        }
+        System.out.print("\n");
+        for (String value: mapVilles.values()){
+            System.out.print(value+" " );
+        }
+        System.out.print("\n");
+        System.out.print(mapVilles.size());
+
+    }
 }
--- src/main/java/maps/MapVilles.java
+++ src/main/java/maps/MapVilles.java
@@ -1,4 +1,47 @@
 package maps;
 
+import list.Ville;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class MapVilles {
+
+    public static void main(String[] args) {
+        Map<String, Ville> villeMap = new HashMap<>();
+
+        villeMap.put("Nice", new Ville("Nice", 343000));
+        villeMap.put("Carcasonne", new Ville("Carcassonne", 47800));
+        villeMap.put("Narbonne", new Ville("Narbonne", 53400));
+        villeMap.put("Lyon", new Ville("Lyon", 484000));
+        villeMap.put("Foix", new Ville("Foix", 9700));
+        villeMap.put("Pau", new Ville("Pau", 77200));
+        villeMap.put("Marseille", new Ville("Marseille", 850700));
+        villeMap.put("Tarbes", new Ville("Tarbes", 40600));
+
+        for (Ville ville : villeMap.values()) {
+            System.out.println(ville);
+        }
+
+        String villeMoinsPeuplee = null;
+        int populationMin = Integer.MAX_VALUE;
+
+        for (Map.Entry<String, Ville> ville : villeMap.entrySet()) {
+            if (ville.getValue().getNbHabitant() < populationMin) {
+                populationMin = ville.getValue().getNbHabitant();
+                villeMoinsPeuplee = ville.getKey();
+            }
+        }
+        if (villeMoinsPeuplee != null) {
+            villeMap.remove(villeMoinsPeuplee);
+            System.out.println("\nLa ville la moins peuplée est " + villeMoinsPeuplee + " !\n");
+
+            System.out.println("Villes sans "+ villeMoinsPeuplee +":");
+            for (Ville ville : villeMap.values()) {
+                System.out.println(ville);
+            }
+        }
+    }
 }
+
