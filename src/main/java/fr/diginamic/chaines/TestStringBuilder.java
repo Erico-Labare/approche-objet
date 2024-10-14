1d0b2f17e24fba8c43ff628e6eb18549603b88ab
Erico Labare <erico.labare@outlook.com>
2024-10-14 12:05:45 +0200

    update TP16

--- src/main/java/fr/diginamic/chaines/TestStringBuilder.java
+++ src/main/java/fr/diginamic/chaines/TestStringBuilder.java
@@ -1,6 +1,21 @@
-package fr.diginamic.stringBuilder;
+package fr.diginamic.chaines;
 
 public class TestStringBuilder {
+    public static void main(String[] args) {
+        long debut = System.currentTimeMillis();
+        StringBuilder nombre1A100k = new StringBuilder();
+        for (int i = 0; i <= 100000; i++) {
+            nombre1A100k.append(i);
+        }
+        long fin = System.currentTimeMillis();
+        System.out.println("Temps écoulé pour StringBuilder en millisecondes :" + (fin - debut));
 
-    
+        long debut2 = System.currentTimeMillis();
+        String string = "";
+        for (int i = 1; i <= 100_000; i++) {
+            string += i;
+        }
+        long fin2 = System.currentTimeMillis();
+        System.out.println("Temps écoulé pour String en millisecondes : " + (fin2 - debut2));
+    }
 }
